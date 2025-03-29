beb66dd7e45588ff03ce9850de4cdf6abb5524ca
minpeter <kali2005611@gmail.com>
2025-03-29 18:07:39 +0900

    replace Sanic with a custom HTTP server implementation and remove requirements.txt

--- api/index.py
+++ api/index.py
@@ -1,9 +1,11 @@
-from sanic import Sanic
-from sanic.response import json
-app = Sanic()
- 
- 
-@app.route('/')
-@app.route('/<path:path>')
-async def index(request, path=""):
-    return json({'hello': path})
+from http.server import BaseHTTPRequestHandler
+
+
+class handler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
+        self.wfile.write("Hello, world!".encode("utf-8"))
+        return
