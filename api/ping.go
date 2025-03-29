b2712ce2f8aacff45bbf961a91842ceed592f364
minpeter <kali2005611@gmail.com>
2025-03-29 17:48:31 +0900

    refactor ping handler and update package name to api

--- api/ping.go
+++ api/ping.go
@@ -1,10 +1,10 @@
-package handler
+package api
 
 import (
 	"fmt"
 	"net/http"
 )
 
-func Handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
+func PingHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "pong")
 }
